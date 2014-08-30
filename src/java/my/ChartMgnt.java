8368b59f308e84e602814bf208fe03b1931ab324
nitinvavdiya <nitinvavdiya@gmail.com>
2014-08-30 09:24:13 +0530

    vnfmnvbndbvndb

--- src/java/my/ChartMgnt.java
+++ src/java/my/ChartMgnt.java
@@ -20,16 +20,7 @@ import util.ResultSetWrapper;
 @WebServlet(name = "ChartMgnt", urlPatterns = {"/ChartMgnt"})
 public class ChartMgnt extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP
-     * <code>GET</code> and
-     * <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+   
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -78,7 +69,10 @@ public class ChartMgnt extends HttpServlet {
             }
             
             
-            ///this is test for amit
+            ///this is
+            System.out.println("test");
+            
+                ///this is test for amit
             System.out.println("test");
            
         }catch(Exception e){
